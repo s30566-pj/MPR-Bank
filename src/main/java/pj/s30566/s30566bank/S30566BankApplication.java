42a1ba361347a46a4f2e60d265ee7153d0eda404
Daniel Makowski <s30566@pjwstk.edu.pl>
2025-01-25 13:22:08 +0100

    initial commit

---
+++ src/main/java/pj/s30566/s30566bank/S30566BankApplication.java
@@ -0,0 +1,13 @@
+package pj.s30566.s30566bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S30566BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S30566BankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pj/s30566/s30566bank/S30566BankApplicationTests.java
@@ -0,0 +1,13 @@
+package pj.s30566.s30566bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S30566BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
